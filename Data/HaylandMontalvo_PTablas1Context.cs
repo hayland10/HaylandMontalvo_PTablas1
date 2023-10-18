e5120304630e414cf9f6794f48a863f6f886cf72
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2023-10-18 10:01:51 -0500

    Agregar archivos de proyecto.

---
+++ Data/HaylandMontalvo_PTablas1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HaylandMontalvo_PTablas1.Models;
+
+namespace HaylandMontalvo_PTablas1.Data
+{
+    public class HaylandMontalvo_PTablas1Context : DbContext
+    {
+        public HaylandMontalvo_PTablas1Context (DbContextOptions<HaylandMontalvo_PTablas1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HaylandMontalvo_PTablas1.Models.Burger> Burger { get; set; } = default!;
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HaylandMontalvo_PTablas1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
